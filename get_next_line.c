169cac3205d73c71765b428b2c90b4badbfd8301
Adam Syed <asyed@e1z2r5p25.42.us.org>
2017-10-20 20:37:24 -0700

    Finished

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/08 16:33:49 by asyed             #+#    #+#             */
-/*   Updated: 2017/10/20 12:40:23 by asyed            ###   ########.fr       */
+/*   Updated: 2017/10/20 20:33:57 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,127 +18,86 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#define FT_EOF 0x1A
-#define CHAR sizeof(char)
 
-int		ft_endline(char *str)
+int		line_split(char **overflow, char **line, int *count)
 {
 	int i;
 
 	i = 0;
-	while (str[i])
+	while ((*line)[i])
 	{
-		if (str[i] == '\n' || str[i] == FT_EOF)
-			return (i);
+		if ((*line)[i] == '\n')
+		{
+			if ((*line)[i + 1])
+			{
+				*overflow = (char *)ft_memalloc(*count - i * SCHAR);
+				*overflow = ft_strncpy(*overflow, &(*line)[i + 1], *count - i);
+			}
+			(*line)[i] = '\0';
+			return (1);
+		}
 		i++;
 	}
 	return (0);
 }
 
-int		empty_check(char *str)
+int		storage_contains(char **overflow, char **line)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] != '\n')
-			return (0);
-		i++;
-	}
-	return (i ? 1 : 0);
-}
-
-int		storage_copy(char **line, char **storage, int *count)
-{
-	char	*save;
+	int		i;
 	int		length;
- 
-	*count = ft_endline(*storage);
-	if (*count)
+	char	*save;
+
+	i = 0;
+	while ((*overflow)[i])
 	{
-		*line = (char *)ft_memalloc((*count + BUFF_SIZE + 1) * CHAR);
-		if (!*line)
-			return (-1);
-		ft_memcpy(*line, *storage, *count * CHAR);
-		length = ft_strlen(*storage);
-		save = (char *)ft_memalloc((length - *count) * CHAR);
-		ft_memcpy(save, &(*storage)[*count + 1], (length - *count - 1) * CHAR);
-		free(*storage);
-		*storage = save;
-		return (1);
+		if ((*overflow)[i] == '\n')
+		{
+			length = ft_strlen(*overflow);
+			save = ft_memalloc(length - i * SCHAR);
+			save = ft_strcpy(save, &(*overflow[i + 1]));
+			*line = ft_strncpy(*line, *overflow, i);
+			(*line)[i] = '\0';
+			free(*overflow);
+			*overflow = save;
+			return (1);
+		}
+		i++;
 	}
-	*count = ft_strlen(*storage);
-	*line = (char *)ft_memalloc((*count + BUFF_SIZE + 1) * CHAR);
-	if (!*line)
-		return (-1);
-	*line = ft_strcpy(*line, *storage);
-	free(*storage);
-	*storage = NULL;
 	return (0);
 }
+
+/*
+** ToDo:
+** Chanage (*line)[i] to resize memory properly to
+** prevent shitty memory usage habits.
+** Error handle all mallocs, etc.
+*/
 
 int		get_next_line(const int fd, char **line)
 {
+	static	char	**overflow;
 	int				r;
 	int				*count;
-	int				found;
-	static	char	**overflow;
-	int				length;
-
-	printf("\n============\nEntered New loop\n============\n");
+	int				res;
 
 	count = ft_memalloc(sizeof(int));
 	if (fd < 0 || !line)
 		return (-1);
 	if (!overflow)
-		if (!(overflow = (char **)ft_memalloc(FDS * sizeof(char *))))
-			return (-1);
-	if (overflow[fd])
-	{
-		if ((r = storage_copy(line, &overflow[fd], count)))
-			return (r);
-	}
-	else
-	{
-		if (!(*line = (char *)ft_memalloc((BUFF_SIZE + 1) * CHAR)))
-			return (-1);
-	}
-	printf("String in func = \"%s\"\n", *line);
-	while ((r = read(fd, &(*line)[*count], BUFF_SIZE)) > 0)
-	{
-		*count += r;
-		if ((found = ft_endline(*line)) > 0)
+		overflow = (char **)ft_memalloc(FDS * sizeof(char *));
+	*line = (char *)ft_memalloc(BUFF_SIZE + 1 * SCHAR);
+	if (overflow[fd] && (res = storage_contains(&(overflow[fd]), line)))
+		return (res);
+	while ((r = read(fd, &(*line)[*count], BUFF_SIZE)) > 0 && (*count += r))
+		if ((res = line_split(&overflow[fd], line, count)))
+			return (res);
+		else if (r == BUFF_SIZE)
 		{
-			length = 0;
-			if (overflow[fd])
-			{
-				length = ft_strlen(overflow[fd]);
-				overflow[fd] = (char *)ft_realloc(overflow[fd], (length +
-								(*count - found) * CHAR));
-				ft_bzero(&overflow[fd][length], ((*count - found) * CHAR));
-			}
-			else
-				overflow[fd] = (char *)ft_memalloc((*count - found) * CHAR);
-			ft_memcpy(&overflow[fd][length], &(*line)[found + 1],
-						(*count - found - 1) * CHAR);
-			(*line)[found] = '\0';
-			printf("Returned here: Str = [\nReturned string\n\"%s\"\n]\n", *line);
-			return (1);
+			*line = (char *)ft_realloc(*line, (*count + BUFF_SIZE + 1) * SCHAR);
+			ft_bzero(&(*line)[*count], BUFF_SIZE + 1 * SCHAR);
 		}
-		if (r == BUFF_SIZE)
-		{
-			*line = (char *)ft_realloc(*line, (*count + BUFF_SIZE) * CHAR);
-			ft_bzero(&(*line)[*count], BUFF_SIZE);
-		}
-	}
 	if (r < 0)
 		return (-1);
-	if (empty_check(*line))
-	{
-		(*line)[*count - 1] = '\0';
-		return (1);
-	}
 	(*line)[*count] = '\0';
 	return (**line ? 1 : 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,16 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/08 16:33:57 by asyed             #+#    #+#             */
-/*   Updated: 2017/10/19 12:35:45 by asyed            ###   ########.fr       */
+/*   Updated: 2017/10/20 20:30:29 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <strings.h>
-# define BUFF_SIZE 30
+# define BUFF_SIZE 1
 # define FDS 256
+# define SCHAR sizeof(char)
 
 int				get_next_line(const int fd, char **line);
 void			*ft_memalloc(size_t size);
@@ -23,5 +24,6 @@ void			*ft_memcpy(void *dest, const void *src, size_t n);
 size_t			ft_strlen(const char *s);
 char			*ft_strcpy(char *dest, const char *src);
 void			ft_bzero(void *s, size_t n);
+char			*ft_strncpy(char *dest, const char *src, size_t n);
 
 #endif
