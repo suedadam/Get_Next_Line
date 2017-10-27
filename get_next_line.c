50585d0686b40d792c088ea3369ab6caf58fd926
Adam Syed <asyed@e1z2r5p25.42.us.org>
2017-10-27 14:47:08 -0700

    Fix norm and everything works

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/08 16:33:49 by asyed             #+#    #+#             */
-/*   Updated: 2017/10/27 13:54:42 by asyed            ###   ########.fr       */
+/*   Updated: 2017/10/27 14:45:55 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,58 +22,36 @@
 int		storage_contains(char **overflow, char **line, int *count)
 {
 	int		i;
-	int		length;
 	char	*save;
 
 	i = 0;
-	// printf("Whatever\n");
-	// printf("Overflow (%p)\n", *overflow);
-	// printf("(%p) Char was: \"%c\"\n", *overflow, (*overflow)[i]);
 	while ((*overflow)[i])
-	{
-		// printf("Entered with overflow[%d] = %c\n", i, (*overflow)[i]);
 		if ((*overflow)[i] == '\n')
 		{
-			// printf("New Line detected.\n");
-			length = ft_strlen(*overflow);
-			save = ft_memalloc((length - i) * SCHAR);
+			save = ft_memalloc((ft_strlen(*overflow) - i) * SCHAR);
 			save = ft_strcpy(save, &((*overflow)[i + 1]));
-			// save = ft_strcpy(save, (*overflow + i));
-			// printf("wow length - i = %d\n", length - i);
-			// printf("save = \"%s\"\n", save);
 			*line = ft_strncpy(*line, *overflow, i);
-			// printf("Line = \"%s\" and Save = \"%s\"\n", *line, save);
-			// printf("Char at [%d] is \"%c\"\n", i, save[i - 2]);
-			(*line)[i] = '\0';
+			(*line)[i++] = '\0';
 			free(*overflow);
 			*overflow = save;
-			// printf("Overflow is now set to %p\n", *overflow);
 			return (1);
 		}
-		// printf("Didn't pass [%d]\n", i);
-		i++;
-	}
-	// printf("Got broken out.\n");
-	// printf("Char was: (%d) \"%c\"\n", i, (*overflow)[i]);
+		else
+			i++;
 	*count += i;
 	*line = ft_strncpy(*line, *overflow, i);
-	// printf("Line = \"%s\"\n", *line);
 	*line = (char *)ft_realloc(*line, (*count + BUFF_SIZE + 1) * SCHAR);
 	ft_bzero((*line + *count), BUFF_SIZE * SCHAR);
-	// printf("Line = \"%s\"\n", *line);
 	free(*overflow);
-	// printf("Free'd\n");
 	*overflow = NULL;
-	// printf("Set it to NULL\n");
 	return (0);
 }
 
-int 	line_split(char **overflow, char **line, int *count)
+int		line_split(char **overflow, char **line, int *count)
 {
 	int i;
 
 	i = 0;
-
 	while ((*line)[i])
 	{
 		if ((*line)[i] == '\n')
@@ -94,8 +72,8 @@ int 	line_split(char **overflow, char **line, int *count)
 
 int		get_next_line(const int fd, char **line)
 {
-	static 	char 	**overflow;
-	int 			r;
+	static	char	**overflow;
+	int				r;
 	int				*count;
 	int				res;
 
@@ -105,27 +83,18 @@ int		get_next_line(const int fd, char **line)
 	if (!overflow)
 		overflow = (char **)ft_memalloc(FDS * sizeof(char *));
 	*line = (char *)ft_memalloc((BUFF_SIZE + 1) * SCHAR);
-	if (overflow[fd])
-	{
-		if ((res = storage_contains(&(overflow[fd]), line, count)))
-			return (res);
-	}
-	while ((r = read(fd, (*line + *count), BUFF_SIZE)) > 0)
-	{
-		*count += r;
-		(*line)[*count] = '\0';
+	if (overflow[fd] && (res = storage_contains(&(overflow[fd]), line, count)))
+		return (res);
+	while ((r = read(fd, (*line + *count), BUFF_SIZE)) > 0 && (*count += r) &&
+			!((*line)[*count] = '\0'))
 		if ((res = line_split(&(overflow[fd]), line, count)))
 			return (res);
-		// else
-			// printf("No new line found.\n");
-		if (r == BUFF_SIZE)
+		else if (r == BUFF_SIZE)
 		{
 			*line = (char *)ft_realloc(*line, (*count + BUFF_SIZE + 1) * SCHAR);
 			ft_bzero((*line + *count), (BUFF_SIZE + 1) * SCHAR);
 		}
-	}
-	if (r < 0)
+	if (r < 0 || ((*line)[*count] = '\0'))
 		return (-1);
-	(*line)[*count] = '\0';
 	return (**line ? 1 : 0);
 }
